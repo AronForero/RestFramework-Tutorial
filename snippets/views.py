7fe4461362d82ab7f71d9dc790b7d8a2f1271b18
Aron Forero <Foreroo965@gmail.com>
2018-12-31 08:57:39 -0500

    Fixed the snippets error that was nos allowing to save the owner into a new snippet.

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,7 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
 # Create your views here.
 
 class snippet_list(generics.ListCreateAPIView):
@@ -9,6 +10,10 @@ class snippet_list(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class snippet_detail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -16,3 +21,12 @@ class snippet_detail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
